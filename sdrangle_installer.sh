d12bf90d0da7b4902fd8d916d880f188a676453f
justin <cerealkiller1918@gmail.com>
2019-12-07 00:06:49 -0600

    clean up more formating

--- sdrangle_installer.sh
+++ sdrangle_installer.sh
@@ -22,12 +22,12 @@ sudo chown $USER:$USER /opt/install
 # install prerequisrites
 sudo apt-get update 
 
-sudo apt-get -y install git cmake g++ pkg-config autoconf automake\
- libtool libfftw3-dev libusb-1.0-0-dev libusb-dev qt5-default qtbase5-dev\
- qtchooser libqt5multimedia5-plugins qtmultimedia5-dev libqt5websockets5-dev\
- qttools5-dev qttools5-dev-tools libqt5opengl5-dev qtbase5-dev libboost-all-dev\
- libasound2-dev pulseaudio libopencv-dev libxml2-dev bison flex ffmpeg\ 
- libavcodec-dev libavformat-dev libopus-dev
+sudo apt-get -y install git cmake g++ pkg-config autoconf automake \
+libtool libfftw3-dev libusb-1.0-0-dev libusb-dev qt5-default qtbase5-dev \
+qtchooser libqt5multimedia5-plugins qtmultimedia5-dev libqt5websockets5-dev \
+qttools5-dev qttools5-dev-tools libqt5opengl5-dev qtbase5-dev libboost-all-dev \
+libasound2-dev pulseaudio libopencv-dev libxml2-dev bison flex ffmpeg \ 
+libavcodec-dev libavformat-dev libopus-dev
 
 
 
@@ -68,7 +68,13 @@ git clone https://github.com/f4exb/dsdcc.git
 cd dsdcc
 #git reset --hard "v1.8.6"
 mkdir build; cd build
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/dsdcc -DUSE_MBELIB=ON -DLIBMBE_INCLUDE_DIR=/opt/install/mbelib/include -DLIBMBE_LIBRARY=/opt/install/mbelib/lib/libmbe.so -DLIBSERIALDV_INCLUDE_DIR=/opt/install/serialdv/include/serialdv -DLIBSERIALDV_LIBRARY=/opt/install/serialdv/lib/libserialdv.so ..
+cmake \
+-Wno-dev \
+-DCMAKE_INSTALL_PREFIX=/opt/install/dsdcc \
+-DUSE_MBELIB=ON -DLIBMBE_INCLUDE_DIR=/opt/install/mbelib/include \
+-DLIBMBE_LIBRARY=/opt/install/mbelib/lib/libmbe.so \
+-DLIBSERIALDV_INCLUDE_DIR=/opt/install/serialdv/include/serialdv \
+-DLIBSERIALDV_LIBRARY=/opt/install/serialdv/lib/libserialdv.so ..
 make -j $PROC install
 
 
@@ -79,7 +85,9 @@ git clone https://github.com/drowe67/codec2.git
 cd codec2
 #git reset --hard 76a20416d715ee06f8b36a9953506876689a3bd2
 mkdir build_linux; cd build_linux
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/codec2 ..
+cmake \
+-Wno-dev \
+-DCMAKE_INSTALL_PREFIX=/opt/install/codec2 ..
 make -j $PROC install
 
 
@@ -89,7 +97,9 @@ cd /opt/build
 git clone https://github.com/f4exb/libmirisdr-4.git
 cd libmirisdr-4
 mkdir build; cd build
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/libmirisdr ..
+cmake \
+-Wno-dev \
+-DCMAKE_INSTALL_PREFIX=/opt/install/libmirisdr ..
 make -j $PROC install
 
 # rtl-sdr
@@ -98,7 +108,10 @@ git clone https://github.com/osmocom/rtl-sdr.git librtlsdr
 cd librtlsdr
 #git reset --hard be1d1206bfb6e6c41f7d91b20b77e20f929fa6a7
 mkdir build; cd build
-cmake -Wno-dev -DDETACH_KERNEL_DRIVER=ON -DCMAKE_INSTALL_PREFIX=/opt/install/librtlsdr ..
+cmake \
+-Wno-dev \
+-DDETACH_KERNEL_DRIVER=ON \
+-DCMAKE_INSTALL_PREFIX=/opt/install/librtlsdr ..
 make -j $PROC install
 
 
@@ -107,7 +120,9 @@ git clone https://github.com/analogdevicesinc/libiio.git
 cd libiio
 #git reset --hard 826563e41b5ce9890b75506f672017de8d76d52d
 mkdir build; cd build
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/libiio -DINSTALL_UDEV_RULE=OFF ..
+cmake \
+-Wno-dev \
+-DCMAKE_INSTALL_PREFIX=/opt/install/libiio -DINSTALL_UDEV_RULE=OFF ..
 make -j $PROC install
 
 
@@ -116,7 +131,10 @@ git clone https://github.com/Nuand/bladeRF.git
 cd bladeRF/host
 #git reset --hard "v2018.01.1"
 mkdir build; cd build
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/libhackrf -DINSTALL_UDEV_RULES=OFF ..
+cmake \
+-Wno-dev \
+-DCMAKE_INSTALL_PREFIX=/opt/install/libhackrf \
+-DINSTALL_UDEV_RULES=OFF ..
 make -j $PROC install
 
 
@@ -124,7 +142,9 @@ cd /opt/build
 git clone https://github.com/myriadrf/LimeSuite.git
 cd LimeSuite
 
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/LimeSuite ..
+cmake \
+-Wno-dev \
+-DCMAKE_INSTALL_PREFIX=/opt/install/LimeSuite ..
 make -j $PROC install
 
 
@@ -133,7 +153,9 @@ git clone https://github.com/airspy/airspyhf
 cd airspyhf
 #git reset --hard "1.1.5"
 mkdir build; cd build
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/libairspyhf ..
+cmake \
+-Wno-dev \
+-DCMAKE_INSTALL_PREFIX=/opt/install/libairspyhf ..
 make -j $PROC install
 
 
@@ -143,7 +165,9 @@ cd libperseus-sdr
 git checkout fixes
 #git reset --hard afefa23e3140ac79d845acb68cf0beeb86d09028
 mkdir build; cd build
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/libperseus ..
+cmake \
+-Wno-dev \
+-DCMAKE_INSTALL_PREFIX=/opt/install/libperseus ..
 make
 make - j $PROC install
 
@@ -156,7 +180,9 @@ git submodule init
 git submodule update
 cd sources
 mkdir build; cd build
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/xtrx-images -DENABLE_SOAPY=NO ..
+cmake \
+-Wno-dev \
+-DCMAKE_INSTALL_PREFIX=/opt/install/xtrx-images -DENABLE_SOAPY=NO ..
 make
 make -j $PROC install
 
@@ -166,7 +192,8 @@ git clone https://github.com/pothosware/SoapySDR.git
 cd SoapySDR
 #it reset --hard "soapy-sdr-0.7.1"
 mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=/opt/install/SoapySDR ..
+cmake \
+-DCMAKE_INSTALL_PREFIX=/opt/install/SoapySDR ..
 make -j $PROC install
 
 
@@ -174,7 +201,12 @@ cd /opt/build
 git clone https://github.com/pothosware/SoapyRTLSDR.git
 cd SoapyRTLSDR
 mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=/opt/install/SoapySDR  -DRTLSDR_INCLUDE_DIR=/opt/install/librtlsdr/include -DRTLSDR_LIBRARY=/opt/install/librtlsdr/lib/librtlsdr.so -DSOAPY_SDR_INCLUDE_DIR=/opt/install/SoapySDR/include -DSOAPY_SDR_LIBRARY=/opt/install/SoapySDR/lib/libSoapySDR.so ..
+cmake \
+-DCMAKE_INSTALL_PREFIX=/opt/install/SoapySDR  \
+-DRTLSDR_INCLUDE_DIR=/opt/install/librtlsdr/include \
+-DRTLSDR_LIBRARY=/opt/install/librtlsdr/lib/librtlsdr.so \
+-DSOAPY_SDR_INCLUDE_DIR=/opt/install/SoapySDR/include \
+-DSOAPY_SDR_LIBRARY=/opt/install/SoapySDR/lib/libSoapySDR.so ..
 make -j $PROC install
 
 
@@ -182,13 +214,21 @@ cd /opt/build
 git clone https://github.com/pothosware/SoapyHackRF.git
 cd SoapyHackRF
 mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=/opt/install/SoapySDR -DLIBHACKRF_INCLUDE_DIR=/opt/install/libhackrf/include/libhackrf -DLIBHACKRF_LIBRARY=/opt/install/libhackrf/lib/libhackrf.so -DSOAPY_SDR_INCLUDE_DIR=/opt/install/SoapySDR/include -DSOAPY_SDR_LIBRARY=/opt/install/SoapySDR/lib/libSoapySDR.so ..
+cmake \
+-DCMAKE_INSTALL_PREFIX=/opt/install/SoapySDR \
+-DLIBHACKRF_INCLUDE_DIR=/opt/install/libhackrf/include/libhackrf \
+-DLIBHACKRF_LIBRARY=/opt/install/libhackrf/lib/libhackrf.so \
+-DSOAPY_SDR_INCLUDE_DIR=/opt/install/SoapySDR/include \
+-DSOAPY_SDR_LIBRARY=/opt/install/SoapySDR/lib/libSoapySDR.so ..
 make -j $PROC install
 
 
 cd /opt/build
 cd LimeSuite/builddir
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/LimeSuite -DCMAKE_PREFIX_PATH=/opt/install/SoapySDR ..
+cmake \
+-Wno-dev \
+-DCMAKE_INSTALL_PREFIX=/opt/install/LimeSuite \
+-DCMAKE_PREFIX_PATH=/opt/install/SoapySDR ..
 make -j $PROC install
 cp /opt/install/LimeSuite/lib/SoapySDR/modules0.7/libLMS7Support.so /opt/install/SoapySDR/lib/SoapySDR/modules0.7
 
@@ -199,7 +239,10 @@ git clone https://github.com/pothosware/SoapyRemote.git
 cd SoapyRemote
 #git reset --hard "soapy-remote-0.5.1"
 mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=/opt/install/SoapySDR -DSOAPY_SDR_INCLUDE_DIR=/opt/install/SoapySDR/include -DSOAPY_SDR_LIBRARY=/opt/install/SoapySDR/lib/libSoapySDR.so ..
+cmake \
+-DCMAKE_INSTALL_PREFIX=/opt/install/SoapySDR \
+-DSOAPY_SDR_INCLUDE_DIR=/opt/install/SoapySDR/include \
+-DSOAPY_SDR_LIBRARY=/opt/install/SoapySDR/lib/libSoapySDR.so ..
 make -j $PROC install
 
 
@@ -222,7 +265,12 @@ git clone https://github.com/pothosware/SoapySDRPlay.git
 cd SoapySDRPlay
 #git reset --hard "soapy-sdrplay-0.2.0"
 mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=/opt/install/SoapySDR -DLIBSDRPLAY_INCLUDE_DIRS=/opt/install/libsdrplay/include -DLIBSDRPLAY_LIBRARIES=/opt/install/libsdrplay/lib/libmirsdrapi-rsp.so -DSOAPY_SDR_INCLUDE_DIR=/opt/install/SoapySDR/include -DSOAPY_SDR_LIBRARY=/opt/install/SoapySDR/lib/libSoapySDR.so ..
+cmake \
+-DCMAKE_INSTALL_PREFIX=/opt/install/SoapySDR \
+-DLIBSDRPLAY_INCLUDE_DIRS=/opt/install/libsdrplay/include \
+-DLIBSDRPLAY_LIBRARIES=/opt/install/libsdrplay/lib/libmirsdrapi-rsp.so \
+-DSOAPY_SDR_INCLUDE_DIR=/opt/install/SoapySDR/include \
+-DSOAPY_SDR_LIBRARY=/opt/install/SoapySDR/lib/libSoapySDR.so ..
 make -j $PROC install
 
 
